c86d405e04fb1a6d99e8b060a15404afb207f0a3
AbderrahmaneGuermat <agb139@alu.ua.es>
2022-01-05 14:37:15 +0100

    Plantilla microservicios de referencia

---
+++ skeleton/ApiGateway-Ejemplo/src/main/java/com/microservicios/apigatewayejemplo/ApiGateWayAppExample.java
@@ -0,0 +1,13 @@
+package com.microservicios.apigatewayejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayAppExample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayAppExample.class, args);
+	}
+
+}
---
+++ skeleton/Servicio-Ejemplo/src/main/java/com/microservicios/servicioejemplo/ServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios.servicioejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceExampleApplication.class, args);
+	}
+
+}
